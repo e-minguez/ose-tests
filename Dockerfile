55f84dd4ef1a3b177954b8806b2bdfe1ffea16a6
Eduardo Minguez Perez <e.minguez@gmail.com>
2020-06-22 16:47:08 +0200

    Diff file

--- Dockerfile
+++ Dockerfile
@@ -8,7 +8,7 @@ COPY allobjects.sh apigetter.sh runtests-local.sh /usr/local/bin/
 COPY *.txt /usr/local/share/ose-tests/
 
 RUN yum install -y https://dl.fedoraproject.org/pub/epel/epel-release-latest-7.noarch.rpm && \
-    yum install -y jq parallel python27-python-pip && \
+    yum install -y jq parallel python27-python-pip git && \
     scl enable python27 "pip install junit2html" && \
     chmod a+x /usr/local/bin/{allobjects.sh,apigetter.sh,runtests-local.sh}
 
--- runtests-local.sh
+++ runtests-local.sh
@@ -18,7 +18,12 @@ for file in /usr/local/share/ose-tests/*.txt; do
   # If some tests fail, continue the execution
   /usr/bin/openshift-tests run --junit-dir="${DESTDIR}"/ -f "${file}" -o "${DESTDIR}"/"${file##*/}" || true
 done
+# Wait some seconds for temporary namespaces to be deleted
+sleep 10
 /usr/local/bin/allobjects.sh > "${DESTDIR}"/after.out
 scl enable python27 "junit2html ${DESTDIR}/junit*.xml"
+
+# Generate a diff file
+git diff --word-diff-regex="[^[:space:]:]+" "${DESTDIR}"/before.out "${DESTDIR}"/after.out > "${DESTDIR}"/diff.out
 
 exit 0
