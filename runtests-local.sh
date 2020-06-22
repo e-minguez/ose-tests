a8c802b8864414cfb863441d71426ae858a0ff3d
Eduardo Minguez Perez <e.minguez@gmail.com>
2020-06-22 16:15:23 +0200

    Proper folder

--- runtests-local.sh
+++ runtests-local.sh
@@ -9,16 +9,16 @@ if [ ! -f /tests/kubeconfig ]; then
     exit 1
 fi
 
-DATE="$(date +%Y%m%d-%H%M%S)"
-mkdir -p "${DATE}"
+DESTDIR=/tests/"$(date +%Y%m%d-%H%M%S)"
+mkdir -p "${DESTDIR}"
 
 export KUBECONFIG=/tests/kubeconfig 
-/usr/local/bin/allobjects.sh > /tests/"${DATE}"/before.out
+/usr/local/bin/allobjects.sh > "${DESTDIR}"/before.out
 for file in /usr/local/share/ose-tests/*.txt; do
   # If some tests fail, continue the execution
-  /usr/bin/openshift-tests run --junit-dir=/tests/"${DATE}"/ -f "${file}" -o /tests/"${DATE}"/"${file##*/}" || true
+  /usr/bin/openshift-tests run --junit-dir="${DESTDIR}"/ -f "${file}" -o "${DESTDIR}"/"${file##*/}" || true
 done
-/usr/local/bin/allobjects.sh > /tests/"${DATE}"/after.out
-scl enable python27 "junit2html /tests/${DATE}/junit*.xml"
+/usr/local/bin/allobjects.sh > "${DESTDIR}"/after.out
+scl enable python27 "junit2html ${DESTDIR}/junit*.xml"
 
 exit 0
