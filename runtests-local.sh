91b097c17009a23798b61acac0181a932d6ae9c9
Eduardo Minguez Perez <e.minguez@gmail.com>
2020-06-22 15:13:02 +0200

    More robust script

--- runtests-local.sh
+++ runtests-local.sh
@@ -1,8 +1,19 @@
 #!/bin/bash
+if [ ! -d /tests/ ]; then
+    echo "tests directory not found!"
+    exit 1
+fi
+
+if [ ! -f /tests/kubeconfig ]; then
+    echo "KUBECONFIG file not found!"
+    exit 1
+fi
+
 export KUBECONFIG=/tests/kubeconfig 
-/usr/local/bin/allobjects.sh > /tests/before.out
+/usr/local/bin/allobjects.sh > /tests/before-"$(date +%Y%m%d-%H%M%S)".out
 for file in /usr/local/share/ose-tests/*.txt; do
-  /usr/bin/openshift-tests run --junit-dir=/tests/ -f "${file}" -o /tests/"${file##*/}"
+  # If some tests fail, continue the execution
+  /usr/bin/openshift-tests run --junit-dir=/tests/ -f "${file}" -o /tests/"${file##*/}" || true
 done
-/usr/local/bin/allobjects.sh > /tests/after.out
+/usr/local/bin/allobjects.sh > /tests/after-"$(date +%Y%m%d-%H%M%S)".out
 scl enable python27 "junit2html /tests/junit*.xml"
