552b389809ea92428526f8905f1a2f2cd61acb17
Eduardo Minguez Perez <e.minguez@gmail.com>
2020-06-22 17:17:20 +0200

    Better diff

--- runtests-local.sh
+++ runtests-local.sh
@@ -19,11 +19,11 @@ for file in /usr/local/share/ose-tests/*.txt; do
   /usr/bin/openshift-tests run --junit-dir="${DESTDIR}"/ -f "${file}" -o "${DESTDIR}"/"${file##*/}" || true
 done
 # Wait some seconds for temporary namespaces to be deleted
-sleep 10
+sleep 20
 /usr/local/bin/allobjects.sh > "${DESTDIR}"/after.out
 scl enable python27 "junit2html ${DESTDIR}/junit*.xml"
 
 # Generate a diff file
-git diff --word-diff-regex="[^[:space:]:]+" "${DESTDIR}"/before.out "${DESTDIR}"/after.out > "${DESTDIR}"/diff.out
+git diff --unified=0 --word-diff-regex="[^[:space:]:]+" "${DESTDIR}"/before.out "${DESTDIR}"/after.out > "${DESTDIR}"/diff.out
 
 exit 0
