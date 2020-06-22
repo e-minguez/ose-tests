59eead18e024bfaa33286b56f317f4ea5ce681da
Eduardo Minguez Perez <e.minguez@gmail.com>
2020-06-22 16:08:13 +0200

    Better output

--- runtests-local.sh
+++ runtests-local.sh
@@ -9,13 +9,16 @@ if [ ! -f /tests/kubeconfig ]; then
     exit 1
 fi
 
+DATE="(date +%Y%m%d-%H%M%S)"
+mkdir -p "${DATE}"
+
 export KUBECONFIG=/tests/kubeconfig 
-/usr/local/bin/allobjects.sh > /tests/before-"$(date +%Y%m%d-%H%M%S)".out
+/usr/local/bin/allobjects.sh > /tests/"${DATE}"/before.out
 for file in /usr/local/share/ose-tests/*.txt; do
   # If some tests fail, continue the execution
-  /usr/bin/openshift-tests run --junit-dir=/tests/ -f "${file}" -o /tests/"$(date +%Y%m%d-%H%M%S)"-"${file##*/}" || true
+  /usr/bin/openshift-tests run --junit-dir=/tests/"${DATE}"/ -f "${file}" -o /tests/"${DATE}"/"${file##*/}" || true
 done
-/usr/local/bin/allobjects.sh > /tests/after-"$(date +%Y%m%d-%H%M%S)".out
-scl enable python27 "junit2html /tests/junit*.xml"
+/usr/local/bin/allobjects.sh > /tests/"${DATE}"/after.out
+scl enable python27 "junit2html /tests/${DATE}/junit*.xml"
 
 exit 0
