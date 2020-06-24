9ee6a106a4ca7f13918700e9858729e369f2f037
Eduardo Minguez Perez <e.minguez@gmail.com>
2020-06-24 10:36:35 +0200

    Improved output

--- scripts/runtests-local.sh
+++ scripts/runtests-local.sh
@@ -20,12 +20,14 @@ DESTDIR=/tests/"$(date +%Y%m%d-%H%M%S)"
 mkdir -p "${DESTDIR}"
 
 export KUBECONFIG=/tests/kubeconfig 
-/usr/local/bin/allobjects.sh > "${DESTDIR}"/before.out
+# Get all the objects. Redirect stdout to avoid 'notfound' and 'Forbidden' messages as it is not executed as cluster-admin
+/usr/local/bin/allobjects.sh > "${DESTDIR}"/before.out 2> /dev/null
 # If some tests fail, continue the execution
 /usr/bin/openshift-tests run --junit-dir="${DESTDIR}"/ -f "${FILE}" -o "${DESTDIR}"/"${FILE##*/}" || true
 # Wait some seconds for temporary namespaces to be deleted
 sleep 20
-/usr/local/bin/allobjects.sh > "${DESTDIR}"/after.out
+# Get all the objects. Redirect stdout to avoid 'notfound' and 'Forbidden' messages as it is not executed as cluster-admin
+/usr/local/bin/allobjects.sh > "${DESTDIR}"/after.out 2> /dev/null
 scl enable python27 "junit2html ${DESTDIR}/junit*.xml"
 
 # Generate a diff file
